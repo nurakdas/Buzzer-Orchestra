0827924d07f8a11423ea097fb2a0d1a2ed51ae9f
Nursultan Tugolbaev <tnursultan@gmail.com>
2020-05-17 22:18:45 -0700

    ADDED ALL PIANO NOTES. GOT RID OF UNUSED CODE.

--- main.c
+++ main.c
@@ -5,6 +5,47 @@
 
 #define F_CPU 48000000L
 
+// The periods (T = 1/f) of all musical notes on the piano
+// Feel free to copy and use them in your projects
+#define A0  36364
+#define Ad0 34223
+#define B0  32396
+#define C1  30578
+#define Cd1 28862
+#define D1  27242
+#define Dd1 25713
+#define E1  24270
+#define F1  22907
+#define Fd1 21622
+#define G1  20409
+#define Gd1 19263
+#define A1  18182
+#define Ad1 17161
+#define B1  16198
+#define C2  15289
+#define Cd2 14431
+#define D2  13621
+#define Dd2 12856
+#define E2  12135
+#define F2  11454
+#define Fd2 10811
+#define G2  10204
+#define Gd2 9632
+#define A2  9091
+#define Ad2 8581
+#define B2  8099
+#define C3  7645
+#define Cd3 7216
+#define D3  6811
+#define Dd3 6428
+#define E3  6067
+#define F3  5727
+#define Fd3 5406
+#define G3  5102
+#define Gd3 4816
+#define A3  4545
+#define Ad3 4290
+#define B3  4050
 #define C4  3822
 #define Cd4 3608
 #define D4  3405
@@ -25,28 +66,39 @@
 #define F5  1432
 #define Fd5 1351
 #define G5  1276
+#define Gd5 1204
+#define A5  1136
+#define Ad5 1073
+#define B5  1012
+#define C6  956
+#define Cd6 902
+#define D6  851
+#define Dd6 804
+#define E6  758
+#define F6  716
+#define Fd6 676
+#define G6  638
+#define Gd6 602
+#define A6  568
+#define Ad6 536
+#define B6  506
+#define C7  478
+#define Cd7 451
+#define D7  426
+#define Dd7 401
+#define E7  379
+#define F7  358
+#define Fd7 338
+#define G7  319
+#define Gd7 301
+#define A7  284
+#define Ad7 268
+#define B7  253
+#define C8  239
 
 void delay(int dly)
 {
 	while( dly--);
-}
-
-char HexDigit[]="0123456789ABCDEF";
-void PrintNumber(int N, int Base, int digits)
-{
-	int j;
-	#define NBITS 32
-	char buff[NBITS+1];
-	buff[NBITS]=0;
-
-	j=NBITS-1;
-	while ( (N>0) | (digits>0) )
-	{
-		buff[j--]=HexDigit[N%Base];
-		N/=Base;
-		if(digits!=0) digits--;
-	}
-	eputs(&buff[j+1]);
 }
 
 void wait_1us(void) {
@@ -62,6 +114,7 @@ void waitus(int len) {
 	while(len--) wait_1us();
 }
 
+// This is needed because waitus is capped at 255 (STM32F051 is a 16 bit architecture)
 void waitustimer (int period) {
 	int i = 0;
 	int j = 0;
@@ -76,8 +129,7 @@ void waitustimer (int period) {
 	}
 }
 
-void wait_1ms(void)
-{
+void wait_1ms(void) {
 	// For SysTick info check the STM32F0xxx Cortex-M0 programming manual page 85.
 	STK_RVR = (F_CPU/1000L) - 1;  // set reload register, counter rolls over from zero, hence -1
 	STK_CVR = 0; // load the SysTick counter
@@ -86,167 +138,8 @@ void wait_1ms(void)
 	STK_CSR = 0x00; // Disable Systick counter
 }
 
-void waitms(int len)
-{
+void waitms(int len) {
 	while(len--) wait_1ms();
-}
-
-void music (int period) {
-	GPIOA_ODR |= 0x1; // PA0=1
-	waitustimer(period/2);
-	GPIOA_ODR &= ~(0x1); // PA0=0
-	waitustimer(period/2);
-}
-
-void beethoven (int timerlimit) {
-	int i;
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < 4*timerlimit; i++)
-		music(Dd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++) {}
-	for (i = 0; i < timerlimit; i++)
-		music(F4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(F4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(F4);
-	waitms(1);
-	for (i = 0; i < 4*timerlimit; i++)
-		music(D4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++) {}
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Dd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 4*timerlimit; i++)
-		music(C5);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++) {}
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(D4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(Gd4);
-	waitms(1);
-	for (i = 0; i < timerlimit; i++)
-		music(G4);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 6*timerlimit; i++)
-		music(D5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++) {}
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 8*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++) {}
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 8*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++) {}
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 2*timerlimit; i++)
-		music(F5);
-	waitms(1);
-	for (i = 0; i < 8*timerlimit; i++)
-		music(Dd5);
-	waitms(1);
-	for (i = 0; i < 8*timerlimit; i++) {}
-	for (i = 0; i < 8*timerlimit; i++)
-		music(C5);
-	waitms(1);
-	for (i = 0; i < 8*timerlimit; i++) {}
-	for (i = 0; i < 16*timerlimit; i++)
-		music(G5);
-	waitms(1);
-	for (i = 0; i < 16*timerlimit; i++) {}
 }
 
 void buzzer1 (int period) {
@@ -257,9 +150,9 @@ void buzzer1 (int period) {
 }
 
 void buzzer2 (int period) {
-	GPIOB_ODR |= 0x4; // PB2=1
+	GPIOB_ODR |= 0x2; // PB1=1
 	waitustimer(period/2);
-	GPIOB_ODR &= ~(0x4); // PB2=0
+	GPIOB_ODR &= ~(0x2); // PB1=0
 	waitustimer(period/2);
 }
 
@@ -284,40 +177,49 @@ void buzzer5 (int period) {
 	waitustimer(period/2);
 }
 
+void buzzermania (int period) {
+	GPIOA_ODR |= 0x1; // PA0=1
+	GPIOB_ODR |= 0x2; // PB1=1
+	GPIOB_ODR |= 0x80; // PB7=1
+	GPIOA_ODR |= 0x100; // PA8=1
+	GPIOA_ODR |= 0x8000; // PA15=1
+	waitustimer(period/2);
+	GPIOA_ODR &= ~(0x1); // PA0=0
+	GPIOB_ODR &= ~(0x2); // PB1=0
+	GPIOB_ODR &= ~(0x80); // PB7=0
+	GPIOA_ODR &= ~(0x8000); // PA15=0
+	GPIOA_ODR &= ~(0x100); // PA8=0
+	waitustimer(period/2);
+}
+
 void main(void)
 {
 	printf("\e[1;1H\e[2J"); // Clears the screen with each reboot/reset/restart
-	char buff[3], notebuff[3], freqbuff[32];
-	int mode;
-	int customt = 0;
-	int timerlimit = 33;
-	notebuff[0] = '0';
+	char buff[3]; // buff[0]=user input, buff[1]=\, buff[2]=0
+	int mode; // User input, equal to buff[0]
+	int i;
 
 	// Peripheral clock initialisation
-	RCC_AHBENR |= 0x20000; // peripheral clock enable for port A
-	RCC_AHBENR |= 0x40000; // periphreal clock enable for port B
-
-	/*
-	// Information here: http://hertaville.com/stm32f0-gpio-tutorial-part-1.html
-	GPIOA_MODER &= ~(BIT16 | BIT17); // Make pin PA8 input
-	// Activate pull up for pin PA8:
-	GPIOA_PUPDR |= BIT16;
-	GPIOA_PUPDR &= ~(BIT17);
-	*/
+	RCC_AHBENR |= 0x20000; // Peripheral clock enable for port A
+	RCC_AHBENR |= 0x40000; // Periphreal clock enable for port B
 
 	// Sound initialisation
 	GPIOA_MODER |= 0x1; // Make pin PA0 (6) output
-	GPIOB_MODER |= 0x10; // Make pin PB2 (16) output
+	GPIOB_MODER |= 0x4; // Make pin PB1 (15) output
 	GPIOB_MODER |= 0x4000; // Make pin PB7 (30) output
 	GPIOA_MODER |= 0x40000000; // Make pin PA15 (25) output
 	GPIOA_MODER |= 0x10000; // Make pin PA8 (18) output
+	
 	waitms(500); // Wait for putty to start
-
-	printf("\r\n");
+	
+	printf("\r\n"); // Used so that the text doesn't slip down with each reset
 	printf("Please select your music piece:\r\n");
 	printf("1) Beethoven - Symphony no. 5\r\n");
 	printf("Enter 1: ");
 
+	// scanf() substitute for STM32:
+	// Puts the user input into the "buff" array, 
+	// then puts the array content into the "mode" integer
 	fflush(stdout);
 	egets(buff, sizeof(buff)-1);
 	printf("%s\r\n", buff);
@@ -326,8 +228,18 @@ void main(void)
 
 	while(1)
 	{
-		//beethoven(timerlimit);
-		//buzzer1(G4);
-		buzzer4(G4);
+		if (mode == 1) {
+		
+			for (i = 0; i < 33; i++) {buzzer1(C5); buzzer2(C3);}
+			for (i = 0; i < 33; i++) {buzzer1(Dd5); buzzer2(G3);}
+			for (i = 0; i < 33; i++) {buzzer1(D5); buzzer2(F3);}
+			for (i = 0; i < 33; i++) {buzzer1(Dd5); buzzer2(G3);}
+			
+			for (i = 0; i < 33; i++) {buzzer1(C4); buzzer2(Dd3);}
+			for (i = 0; i < 33; i++) {buzzer1(Dd5); buzzer2(G3);}
+			for (i = 0; i < 33; i++) {buzzer1(D5); buzzer2(F3);}
+			for (i = 0; i < 33; i++) {buzzer1(Dd5); buzzer2(G3);}
+			
+		}
 	}
 }
